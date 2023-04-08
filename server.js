36e5b10cf8111f2eb91e739743507d50fe04e792
dev-002 <devansh.gupta985@gmail.com>
2023-04-08 12:01:03 +0530

    Authentication Page Route Setup

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express')
+const ejs = require("ejs")
 
 const app = express();
+
+app.set("view engine", "ejs")
+app.set('views', __dirname + '/views');
+
+//Route
+const indexRoute = require('./routes/index')
+
+app.use('/', indexRoute)
 
 app.listen(process.env.PORT || 5000 , ()=> console.log("Server is Running ...."))
