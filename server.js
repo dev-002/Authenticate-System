21a2e462c95dd8f5af86f699c9ef2af0973c354a
dev-002 <devansh.gupta985@gmail.com>
2023-04-09 17:12:34 +0530

    MongoDB Connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const ejs = require("ejs")
 
 const app = express();
@@ -6,6 +7,17 @@ const app = express();
 app.set("view engine", "ejs")
 app.set('views', __dirname + '/views');
 app.use(express.urlencoded({extended: true}))
+
+// Mongoose Database
+const dburl = process.env.DB_URL || 'mongodb://localhost:27017/Authentication-System'
+mongoose.connect(dburl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, "connection-error"))
+db.once("open", ()=>console.log("Database Connected"))
 
 //Route
 const indexRoute = require('./routes/index')
